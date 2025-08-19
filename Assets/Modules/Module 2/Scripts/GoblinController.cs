a577c640dcc228494a41d08386745d338cddaf17
Jolo Gutierrez <mariano.jose.gutierrez.iii@dlsl.edu.ph>
2025-08-19 11:21:38 +0800

    vector samples

--- Assets/Modules/Module 2/Scripts/GoblinController.cs
+++ Assets/Modules/Module 2/Scripts/GoblinController.cs
@@ -1,37 +1,27 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace DLSL.ResourceCollectorDemo.Module2
 {
-    [RequireComponent(typeof(Rigidbody2D))]
     public class GoblinController : MonoBehaviour
     {
-        [SerializeField] float _moveSpeed = 2f;
-        [SerializeField] float _jumpForce = 10f;
-        InputSystem_Actions _inputs ;
-        private Rigidbody2D _rigidbody;
-        private Vector2 _direction;
-        private void Awake()
+        [SerializeField]float _duration = 1f;
+        Vector2 _targetPosition;
+        float _moveSpeed = 2f;
+        private void Start()
         {
-            _rigidbody = GetComponent<Rigidbody2D>();
-            _inputs = new();
-            _inputs.Player.Enable();
-                    }
-
-        private void OnDestroy()
-        {
-            _inputs.Player.Disable();
+            _targetPosition = Random.insideUnitCircle * 5f;
         }
 
         private void Update()
         {
-            _direction = _inputs.Player.Move.ReadValue<Vector2>();
+            transform.position = Vector2.MoveTowards(transform.position, _targetPosition, _moveSpeed * Time.deltaTime);
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(_targetPosition, 0.1f);
         }
 
-        private void FixedUpdate()
-        {
-            _rigidbody.MovePosition(_rigidbody.position + _direction * _moveSpeed * Time.deltaTime);
-        }
     }
 }
--- Assets/Scripts/Resources/GoldSpawner.cs
+++ Assets/Scripts/Resources/GoldSpawner.cs
@@ -1,10 +1,30 @@
+using UnityEngine;
+
 namespace DLSL.ResourceCollectorDemo.Resources
 {
     public class GoldSpawner : ResourceSpawner
     {
+        [SerializeField] float _spawnRadius;
+        private void Update()
+        {
+            transform.Translate(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * 2f * Time.deltaTime);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SpawnResources(5);    
+            }    
+        }
+
         public override void SpawnResources(int count)
         {
-            print("The spawned resources are Gold");
+            for (int i = 0; i < count; i++)
+            {
+                var spawnPosition =(Vector2)transform.position + Random.insideUnitCircle * _spawnRadius;
+                Instantiate(this.Resource, spawnPosition, Quaternion.identity);
+            }
+        }
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, _spawnRadius);
         }
     }
 }
